0cd440db0485546a81665e58bac006f5ee0e92d3
joehoct <joehoct@lanecrawford.com>
2024-01-25 09:27:26 +0000

    Update monolith deployment script

--- deploy-monolith.sh
+++ deploy-monolith.sh
@@ -14,20 +14,12 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-printf "Enabling Cloud Build APIs...\n"
-gcloud services enable cloudbuild.googleapis.com
-printf "Completed.\n\n"
+printf "Building Image...\n"
+cd ./monolith
+docker build -t monolith:1.0.0 .
 
-printf "Building Monolith Container...\n"
-cd ~/monolith-to-microservices/monolith
-gcloud builds submit --tag gcr.io/${GOOGLE_CLOUD_PROJECT}/monolith:1.0.0 .
-printf "Completed.\n\n"
-
-printf "Deploying Monolith To GKE Cluster...\n"
-kubectl create deployment monolith --image=gcr.io/${GOOGLE_CLOUD_PROJECT}/monolith:1.0.0
-kubectl expose deployment monolith --type=LoadBalancer --port 80 --target-port 8080
-printf "Completed.\n\n"
-
-printf "Please run the following command to find the IP address for the monolith service: kubectl get service monolith\n\n"
-
-printf "Deployment completed successfully!\n"
+printf "Deploying monolith...\n"
+k3d image import monolith:1.0.0
+kubectl apply -f deployment.yaml
+kubectl apply -f service.yml
+kubectl apply -f ingress.yml
